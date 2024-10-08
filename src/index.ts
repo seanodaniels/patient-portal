4b15d7ffed751fb7c64dc2cf2998bbb0f63f4197
Sean O'Daniels <sodaniels@gmail.com>
2024-10-08 12:30:02 -0400

    fix: added cors header info before routes.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import patientorRouter from './routes/patientor'
+
 const app = express()
+
 import cors from 'cors'
 app.use(express.json())
+
+app.use(cors())
 
 // const PORT = 3001
 console.log(`process.env.PORT is ${process.env.PORT}`)
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(function (_req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+})
 
 app.get('/api/ping', (_req, res) => {
     console.log('ping received')
